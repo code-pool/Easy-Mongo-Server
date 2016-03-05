175706b0aef071835abe0265c8d63ca470c56467
Pranay Dubey <pranaydubey3009@gmail.com>
2016-03-05 12:44:34 +0530

    Initial commit for auth  - Add login/logout for controller  - Add login/logout for validator  - Add route handling

---
+++ modules/auth/controller.js
@@ -0,0 +1,35 @@
+var Promise = require('bluebird'),
+    mongoose = Promise.promisifyAll(require('mongoose')),
+    User = mongoose.model('User');
+    authUtils = require('utils/auth');
+
+module.exports = {
+  login : Login,
+  logout : Logout
+};
+
+function Login(request,reply) {
+
+  var user = reply.data,
+    token = authUtils.createToken({'id' : user.id, 'scope' : user.type});
+
+  reply.data = {
+    'token' : token,
+    'id' : user.id
+  };
+
+  reply.next();
+
+}
+
+function Logout(request,reply) {
+
+  authUtils.clearToken(request.payload.token)
+           .then(function() {
+            reply.next();          
+           })
+           .catch(function(e) {
+            reply.next(e);
+           });
+
+}
---
+++ modules/auth/validator.js
@@ -0,0 +1,49 @@
+var Promise = require('bluebird'),
+  mongoose = Promise.promisifyAll(require('mongoose')),
+  User = mongoose.model('User'),
+  joi = require('joi'),
+  database = require('database'),
+  pattern = require('utils/pattern');
+
+module.exports = {
+
+  login : Login,
+  logout : Logout,
+
+  validateReqLogin : ValidateReqLogin
+
+};
+
+function Login(request,reply) {
+
+  var user = database;
+  if(user.username !== request.payload.username || user.password !== request.payload.password) {
+    return reply.next({'message' : 'User not found', 'status' : 401});    
+  }
+  
+  reply.data = user;
+  reply.next();  
+  
+}
+
+function Logout(request,reply) {
+  
+  if(!request.headers.authorization) {
+    reply.data = {'logout' : true};
+    return reply.next();
+  }
+
+  reply.next();
+
+}
+
+function ValidateReqLogin() {
+  
+  return {
+    'payload' : {
+      'username' : joi.string().required(),
+      'password' : joi.string().required()
+    }
+  };
+}
+
