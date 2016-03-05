cd74018f0d3a1bfe5fc04a91a9729ad04b8c61ba
Pranay Dubey <pranaydubey3009@gmail.com>
2016-03-05 14:04:50 +0530

    Add CORS restrictions on the server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var hapi = require('hapi'),
     server;
 
 server = module.exports = new hapi.Server();        
-server.connection({'port' : process.env.NODE_PORT || 3000});
+server.connection({'port' : process.env.NODE_PORT || 3000,'routes' : {'cors' : { 'origin' : ['http://localhost:8000']}}});
 
 require('database');
 
