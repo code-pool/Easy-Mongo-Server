5546e2f02f703581ca03b270dad15b3ab9bd7483
Pranay Dubey <pranaydubey3009@gmail.com>
2016-03-05 12:23:11 +0530

    Repo structure initial commit  - Get base skeleton from https://github.com/Pranay92/hapi-skeleton

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').load();
+
+var hapi = require('hapi'),
+    server;
+
+server = module.exports = new hapi.Server();        
+server.connection({'port' : process.env.NODE_PORT || 3000});
+
+require('database');
+
+require('modules');
+
+require('bootstrap');
+
+server.start(function() {
+  console.log('Server is up and running');
+});
